9f65d24c2db9af1e748b2edece44186afcfbb510
loveyoun <lovekite0406@naver.com>
2021-06-20 10:07:45 +0900

    TRIVIAL CORRECTIONS2

--- app/src/main/java/com/example/aap/Community/GalleryAdapter.kt
+++ app/src/main/java/com/example/aap/Community/GalleryAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils.centerCrop
 import com.bumptech.glide.module.AppGlideModule
 import com.example.aap.R
 import com.example.aap.databinding.GalleryRowBinding
