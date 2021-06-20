4e92f8800eb6bb00d792835e61a68ff0ff0d348e
loveyoun <lovekite0406@naver.com>
2021-06-20 15:58:52 +0900

    LOADING correction

--- app/src/main/java/com/example/aap/LoadingActivity.kt
+++ app/src/main/java/com/example/aap/LoadingActivity.kt
@@ -4,40 +4,59 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import com.example.aap.databinding.ActivityLoadingBinding
+import java.util.*
+import kotlin.concurrent.schedule
 
 class LoadingActivity : AppCompatActivity() {
     //private val SPLASH_TIME_OUT: Long = 3000
     lateinit var binding : ActivityLoadingBinding
 
-    lateinit var handler: Handler
-    lateinit var runnable: Runnable
+//    lateinit var handler: Handler
+//    lateinit var runnable: Runnable
+    lateinit var timer: Timer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoadingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        handler = Handler()
-        runnable= Runnable {
-            val intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
-        }
-
-        /*이 프로젝트에서는 실행이 안됨.*/
-        binding.skipbtn.setOnClickListener {
-            handler.removeCallbacks(runnable)
-        }
         init()
     }
 
+    private fun skipactivity(){
+        val intent = Intent(this, SignIOActivity::class.java)
+        startActivity(intent)
+    }
 
     private fun init(){
-        handler.postDelayed({
-            val intent = Intent(this, SignIOActivity::class.java)
-            startActivity(intent)
-            finish()
-        },5000)
+        //val handler = Handler(Looper.getMainLooper())
+
+        timer = Timer()
+        timer.schedule(5000){
+            skipactivity()
+        }
+        binding.skipbtn.setOnClickListener {
+            timer.cancel()
+            skipactivity()
+        }
+
+//        handler = Handler()
+//        runnable= Runnable {
+//            val intent = Intent(this, SignInActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        handler.postDelayed({
+//            val intent = Intent(this, SignIOActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        },5000)
+//        /*이 프로젝트에서는 실행이 안됨.*/
+//        binding.skipbtn.setOnClickListener {
+//            handler.removeCallbacks(runnable)
+//        }
 
     }
 
